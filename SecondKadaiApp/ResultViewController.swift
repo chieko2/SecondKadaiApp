da1404ab0ecd5e1aa3f126ceb3262994b8f41ba1
広部千恵子 <hirobechieko@koubuchieko-no-macbook-pro.local>
2018-03-11 12:37:09 +0900

    SecondKadaiAppを追加

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 広部千恵子 on 2018/03/10.
+//  Copyright © 2018年 広部千恵子. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var Label: UILabel!
+    
+    var name = ""
+    
+    // その他のコード.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var TextField: UITextField!
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue,
+    sender: Any?) {
+    // segueから遷移先のResultViewControllerを取得する
+    let resultViewController:ResultViewController =
+        segue.destination as! ResultViewController
+         //遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
 
 }
-
+}
